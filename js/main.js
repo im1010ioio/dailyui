496a70817a5288481227adbe7246aee7f17b581b
Eva <im1010ioio@gmail.com>
2022-12-23 11:18:06 +0800

    更新題目與預設圖片

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 $(function(){
 	var workSubject = [
-		"Sign Up", "Landing Page", "Landing Page (above the fold)", "Calculator", "App Icon", "User Profile", "Settings", "404 page", "Music Player", "Social Share",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
-		"A", "B", "C", "D", "E", "A", "B", "C", "D", "E",
+		// 1 - 10
+		"Sign Up", "Landing Page", "Landing Page (above the fold)", "Calculator", "App Icon",
+		"User Profile", "Settings", "404 page", "Music Player", "Social Share",
+
+		// 11 - 20
+		"Flash Message (Error/Success)", "E-Commerce Shop (Single Item)", "Direct Messaging", "Countdown Timer", "On/Off Switch",
+		"Pop-Up / Overlay", "Email Receipt", "Analytics Chart", "Leaderboard", "Location Tracker",
+
+		// 21 - 30
+		"Home Monitoring Dashboard", "Search", "Onboarding", "Boarding Pass", "TV App",
+		"Subscribe", "Dropdown", "Contact Us", "Map", "Pricing",
+
+		// 31 - 40
+		"File Upload", "Crowdfunding Campaign", "Customize Product", "Car Interface", "Blog Post",
+		"Special Offer", "Weather", "Calendar", "Testimonials", "Recipe",
+
+		// 41 - 50
+		"Workout Tracker", "ToDo List", "Food/Drink Menu", "Favorites", "Info Card",
+		"A", "Activity Feed", "Coming Soon", "Notifications", "Job Listing",
+
+		// 51 - 60
+		"Press Page", "Daily UI logo", "Header Navigation", "Confirm Reservation", "Icon Set",
+		"Breadcrumbs", "Video Player", "Shopping Cart", "Background Pattern", "Color Picker",
+
+		// 61 - 70
+		"Redeem Coupon", "Workout of the Day", "Best of 2015", "Select User Type", "Notes Widget",
+		"Statistics", "Hotel Booking", "Flight Search", "Trending", "Event Listing",
+
+		// 71 - 80
+		"Schedule", "Image Slider", "Virtual Reality", "Download App", "Pre-Order",
+		"Loading...", "Thank You", "Pending Invitation", "Itinerary", "Date Picker",
+
+		// 81 - 90
+		"Status Update", "Form", "Button", "Badge", "Pagination",
+		"Progress Bar", "Tooltip", "Avatar", "Terms of Service", "Create New",
+
+		// 91 - 100
+		"Curated For You", "F.A.Q.", "Splash Screen", "News", "Product Tour",
+		"Currently In-Stock", "Giveaway", "Advertisement", "Categories", "Redesign Daily UI Landing Page",
 	];
 	for (i=100 ;i>0 ; i--){
 		$(".works").append('<div class="work">' +
@@ -30,8 +59,8 @@ $(function(){
 	}
 
 	$(".pic img").load(function(){
-			console.log($(this).attr("src"));
-			console.log($($(this).parent()).attr('class'));
+			// console.log($(this).attr("src"));
+			// console.log($($(this).parent()).attr('class'));
 			//console.log($(this).parent.attr('class'));
 
 			$(this).show();
